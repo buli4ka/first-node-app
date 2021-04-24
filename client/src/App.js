f475f9a71d595312c64d55b4af20217ef0dbe7c5
Andrew <opalkoandrij@gmail.com>
2021-04-25 00:54:28 +0300

    Added new Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react"
+import "materialize-css"
+import { useRoutes } from "./routes";
+import{BrowserRouter as Router} from "react-router-dom"
+
 
 function App() {
-    return ( <h1> Hello </h1>)
+    const routes = useRoutes(false)
+    return ( <Router><div className="container">{routes}</div></Router>)
     }
 
     export default App;
